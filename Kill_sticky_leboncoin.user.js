e6d5a30f0119bae8543cfe79d0706be9ee73d9b2
fonfano <fonfano@gmail.com>
2023-08-01 17:46:24 +0200

    Create Kill_sticky_leboncoin.user.js

---
+++ Kill_sticky_leboncoin.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Kill_sticky_VR_leboncoin
+// @namespace   github.com/fonfano
+// @match       https://www.leboncoin.fr/compte/part/mes-annonces
+// @grant       none
+// @version     0.1
+// @author      -
+// @description 01/08/2023 10:35:45
+// ==/UserScript==
+
+
+// Historique
+// 29-07-2023   0.1   creation :  Kill dirty header when scrolling
+
+
+
+setInterval(kill, 1000);
+
+function kill()  {
+  let toKill = document.querySelector("#mainContent > div:nth-child(2) > div:nth-child(2) > div.src__CenteredWrapper-sc-1bgl6we-0.jvloxK > section > div.styles_DashboardHeader__0YNrp > div._3IX1b._2VRRU > div");
+  if (toKill) toKill.style.display = "none";
+}
